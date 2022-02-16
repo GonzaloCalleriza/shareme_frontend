6af66c8a4e2b68043993851fccbfe2555caa2c2b
gonzalogonzalo <calleriza.gonzalo@gmail.com>
2022-02-16 20:29:22 +0100

    Updated data with categories and createdpin

--- src/components/CreatePin.jsx
+++ src/components/CreatePin.jsx
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { AiOutlineCloudUpload } from 'react-icons/ai';
+import { MdDelete} from 'react-icons/md';
+import { useNavigate } from 'react-router-dom';
 
-const CreatePin = () => {
+import { client } from '../client';
+import Spinner from './Spinner';
+
+// List of categories 
+
+const CreatePin = ({ user }) => {
     return (
         <div>
             Pins
