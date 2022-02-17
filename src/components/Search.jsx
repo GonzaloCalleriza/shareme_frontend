8ef747664adca4db116ec550192dd974898d4460
gonzalogonzalo <calleriza.gonzalo@gmail.com>
2022-02-17 18:35:52 +0100

    Finished search component

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,9 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-const Search = () => {
+import MasonryLayout from './MasonryLayout';
+import { client } from '../client';
+import { feedQUery, searchQuery } from '../utils/data';
+import Spinner from './Sidebar';
+
+const Search = ({ searchTerm }) => {
+
+    const [pins, setPins] = useState(null);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+
+        if(searchTerm) {
+            setLoading(true)
+            const query = searchQuery(searchTerm.toLowerCase());
+            
+            client.fetch(query)
+                .then((data) => {
+                    setPins(data.pins);
+                    setLoading(false);
+                })
+
+        } else {
+            client.fetch(feedQUery)
+                .then((data) => {
+                    setPins(data.pins);
+                    setLoading(false);
+                })
+        }
+
+    }, [searchTerm])
+
     return (
         <div>
-            Pins
+            {loading && <Spinner message='Searching for pins'/>}
+            {pins?.length !==0 && <MasonryLayout pins={pins} />}
+            {pins?.length === 0 && searchTerm !=='' && !loading && (
+                <div className='mt-10 text-center text-xl'>No pins found!</div>
+            )}
         </div>
     )
 
